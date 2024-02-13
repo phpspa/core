8e3788a50b1cafa50d299f5d99cf6f90b7e83828
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-13 11:35:36 -0300

    first commit

---
+++ src/Gaucho.php
@@ -0,0 +1,26 @@
+<?php
+namespace SPA;
+use SPA\Env;
+use SPA\Route;
+class Gaucho{
+	function run($routes=false){
+		new Env(ROOT.'/.env');	
+		ini_set("memory_limit",$_ENV['SITE_MEMORY']);
+		$this->showErrors($_ENV['SITE_ERRORS']);
+		if($routes){
+			new Route($routes);
+		}
+	}
+
+	function showErrors($bool){
+		if($bool){
+			ini_set('display_errors',1);
+			ini_set('display_startup_errors',1);
+			error_reporting(E_ALL);
+		}else{
+			ini_set('display_errors',0);
+			ini_set('display_startup_errors',0);
+			error_reporting(0);
+		}
+	}
+}
